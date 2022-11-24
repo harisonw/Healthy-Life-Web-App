6ca13907fad6c1366e800e494dea8cfb7370c5bf
harisonw <87620631+harisonw@users.noreply.github.com>
2022-11-24 16:17:43 +0000

    Init upload of node user auth

---
+++ controllers/AuthController.js
@@ -0,0 +1,100 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const register = (req, res) => {
+  var email = req.body.email;
+  var password = req.body.password;
+
+  if (
+    !email ||
+    typeof email !== "string" ||
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    ) === false
+  ) {
+    return res.json({ status: "error", error: "Invalid email" });
+  }
+
+  if (!password || typeof password !== "string" || password.length < 8) {
+    return res.json({ status: "error", error: "Invalid password" });
+  }
+
+  bcrypt.hash(password, 10, (err, hashedPassword) => {
+    if (err) {
+      res.json({
+        error: err,
+      });
+    }
+
+    let user = new User({
+      email: email,
+      password: hashedPassword,
+    });
+
+    user
+      .save()
+      .then((user) => {
+        res.json({
+          status: "ok",
+          message: "User added successfully!",
+        });
+      })
+      .catch((error) => {
+        if (error.code === 11000) {
+          console.log("Duplicate key");
+          res.json({
+            status: "error",
+            error: "Email already exists!",
+          });
+        }
+        else {
+          console.log("Error: not duplicate key");
+          res.json({
+            status: "error",
+            message: "An error occurred!",
+            error: error,
+          });
+        } 
+      });
+  });
+};
+
+const login = (req, res, next) => {
+  var email = req.body.email;
+  var password = req.body.password;
+
+  User.findOne({ email: email }).then((user) => {
+    if (user) {
+      bcrypt.compare(password, user.password, function (err, result) {
+        if (err) {
+          res.json({
+            error: err,
+          });
+        }
+        if (result) {
+          let token = jwt.sign({ name: user }, "healthyLifeKey", {
+            expiresIn: "1h",
+          });
+          res.json({
+            message: "Login successful!",
+            token,
+          });
+        } else {
+          res.json({
+            message: "Password does not match!",
+          });
+        }
+      });
+    } else {
+      res.json({
+        message: "No user found!",
+      });
+    }
+  });
+};
+
+module.exports = {
+  register,
+  login,
+};
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const app = express();
+app.use(express.json());
+
+const AuthRoute = require('./routes/auth');
+const port = 3000;
+var db = 'mongodb://127.0.0.1:27017/HealthyLife';
+
+mongoose.connect(db);
+
+app.use('/api/user', AuthRoute);
+
+app.use('/', express.static(path.join(__dirname, 'static')));
+
+app.listen(3000, () => console.log('server listening on port ' + port));
