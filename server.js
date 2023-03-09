8e05781adf95aebbfeddb0f969334a4b12369540
harisonw <87620631+harisonw@users.noreply.github.com>
2023-03-09 15:27:17 +0000

    fix cors

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
 const cookieParser = require("cookie-parser");
 app.use(express.json());
 app.use(cookieParser());
